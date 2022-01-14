d472191caac78822629adeffe30fd4146053153f
cptvictor95 <cpt.victor@hotmail.com>
2022-01-14 20:05:40 -0300

    add eslint config, create header component

---
+++ web/components/header/index.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h3>Header</h3>
+    </header>
+  );
+};
+
+export default Header;
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import Header from "../components/header";
 import Main from "../layouts/Main";
 
 const Home: NextPage = () => {
   return (
     <Main pageTitle="Home">
+      <Header />
       <h1>WhatsApp2</h1>
     </Main>
   );
